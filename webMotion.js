15747fce846c28b7ef58c13f5309aa654800f0e7
Erik Linde <erik.linde@gmail.com>
2014-04-11 13:13:51 -0400

    s

--- webMotion.js
+++ webMotion.js
@@ -17,31 +17,31 @@
 	var keyPresses = [{character: 'dummy1', timeStamp: 100000, timeOutID: null},{character: 'dummy2', timeStamp: 200000, timeOutID: null},{character: 'dummy3', timeStamp: 300000, timeOutID: null}];
 
 	// Initializes certain listeners needed
-	keysurfHelpers.initializeSpecialKeyListeners();
-	keysurfHelpers.initializeFocusBlurListeners();
+	webMotionHelpers.initializeSpecialKeyListeners();
+	webMotionHelpers.initializeFocusBlurListeners();
 	initializeWindowScrollListener();
 	
 	$(document).ready(function() {
 		chrome.runtime.sendMessage({msg: 'get_viewport_height'}, function(response) {
-			keysurfHelpers.viewPortHeight = response;
+			webMotionHelpers.viewPortHeight = response;
 			processLinks();
 		});
 	});
 
 	$(document).on('keydown', 'html', function(e) {
 		var pressedChar = String.fromCharCode(e.keyCode).toLowerCase();
-		if (keysurfHelpers.isAlphanumeric(pressedChar)) {
+		if (webMotionHelpers.isAlphanumeric(pressedChar)) {
 			handleAlphaNumericKeyPress(pressedChar);
 		}
 	});
 
 	function processLinks() {		
 		// Check to make sure we are not on Google, Facebook, Twitter, etc. They have their own shortcut system.
-		if (keysurfHelpers.isDomainAllowed()) {
+		if (webMotionHelpers.isDomainAllowed()) {
 			modifiableLinks = [];
 			modifiableLinksAlt = [];
-			keysurfHelpers.takenAbbreviations = [];
-			keysurfHelpers.takenAbbreviationsAlt = [];
+			webMotionHelpers.takenAbbreviations = [];
+			webMotionHelpers.takenAbbreviationsAlt = [];
 			resetAllLinks();
 			// Gather all the links we (potentially) need to modify
 			modifiableLinks = gatherLegitimateLinks();
@@ -50,22 +50,22 @@
 			// process each link (ie, figure out which letter to be the shortcut, alter the underlying html, etc)
 			
 			for (var i=0; i <= modifiableLinks.length - 1; i++) {
-				var reprocessForAltKey = keysurfHelpers.analyzeAndModifyLink(modifiableLinks[i].linkObj, false);
+				var reprocessForAltKey = webMotionHelpers.analyzeAndModifyLink(modifiableLinks[i].linkObj, false);
 				if (reprocessForAltKey) {
 					modifiableLinksAlt.push(modifiableLinks[i]);
 				}
 			}
 
 			for (var i=0; i <= modifiableLinksAlt.length - 1; i++) {
-				var reprocessForAltKey = keysurfHelpers.analyzeAndModifyLink(modifiableLinksAlt[i].linkObj, true);
+				var reprocessForAltKey = webMotionHelpers.analyzeAndModifyLink(modifiableLinksAlt[i].linkObj, true);
 			}
 		}
 	}
 
 	function handleAlphaNumericKeyPress(pressedChar) {
-		if (keysurfHelpers.noInputFieldsActive()) {
-			// alert(keysurfHelpers.specialCharactersPressed());
-			if (keysurfHelpers.reservedShortcuts.containsString(pressedChar) && (keysurfHelpers.isDomainAllowed() || keysurfHelpers.alwaysPermissibleShortcuts.containsString(pressedChar)) && !(keysurfHelpers.specialCharactersPressed())) {
+		if (webMotionHelpers.noInputFieldsActive()) {
+			// alert(webMotionHelpers.specialCharactersPressed());
+			if (webMotionHelpers.reservedShortcuts.containsString(pressedChar) && (webMotionHelpers.isDomainAllowed() || webMotionHelpers.alwaysPermissibleShortcuts.containsString(pressedChar)) && !(webMotionHelpers.specialCharactersPressed())) {
 				// user pressed one of the 'reserved keys', example hjkl
 				switch(pressedChar)
 				{	
@@ -73,10 +73,10 @@
 					chrome.runtime.sendMessage({msg: 'close_selected_tab'}, function(response) {});
 					break;
 					case 'j':
-					keysurfHelpers.scrollWindow(300);
+					webMotionHelpers.scrollWindow(300);
 					break;
 					case 'k':
-					keysurfHelpers.scrollWindow(-300);
+					webMotionHelpers.scrollWindow(-300);
 					break;
 					case 'b':
 					window.history.go(-1);
@@ -94,7 +94,7 @@
 					break;
 				}
 			}	
-			else if (keysurfHelpers.isDomainAllowed() && keysurfHelpers.getKeymap(keysurfHelpers.altPressed)[pressedChar] != null && (!(keysurfHelpers.specialCharactersPressed()) || keysurfHelpers.altPressed)) {
+			else if (webMotionHelpers.isDomainAllowed() && webMotionHelpers.getKeymap(webMotionHelpers.altPressed)[pressedChar] != null && (!(webMotionHelpers.specialCharactersPressed()) || webMotionHelpers.altPressed)) {
 				// user pressed 'red' key (ie not one of the reserved keys)
 				// first push the key to the keylog
 				resetAllTimeOuts();
@@ -108,7 +108,7 @@
 					
 					var timeOutID = window.setTimeout(function(localChar) {
 						resetKeyPresses();
-						window.location = keysurfHelpers.getKeymapValue(localChar, keysurfHelpers.altPressed);	
+						window.location = webMotionHelpers.getKeymapValue(localChar, webMotionHelpers.altPressed);	
 					}, 300, pressedChar);
 					keyPresses[keyPresses.length - 1].timeOutID = timeOutID;
 				}
@@ -118,7 +118,7 @@
 						var timeOutID = window.setTimeout(function(localChar) {
 							// open in new tab, and follow
 							resetKeyPresses();
-							chrome.runtime.sendMessage({msg: 'new_tab_follow', url: keysurfHelpers.getKeymapValue(pressedChar, keysurfHelpers.altPressed)}, function(response) {});
+							chrome.runtime.sendMessage({msg: 'new_tab_follow', url: webMotionHelpers.getKeymapValue(pressedChar, webMotionHelpers.altPressed)}, function(response) {});
 						}, 300, pressedChar);
 						keyPresses[keyPresses.length - 1].timeOutID = timeOutID;
 					}
@@ -127,7 +127,7 @@
 						// set timeout to follow link in THIS tab.
 						var timeOutID = window.setTimeout(function(localChar) {
 							resetKeyPresses();
-							window.location = keysurfHelpers.getKeymapValue(localChar, keysurfHelpers.altPressed);
+							window.location = webMotionHelpers.getKeymapValue(localChar, webMotionHelpers.altPressed);
 						}, 300, pressedChar);
 						keyPresses[keyPresses.length - 1].timeOutID = timeOutID;
 					}
@@ -138,14 +138,14 @@
 						// clear keypresses + open up in new tab (don't follow tab)
 						// no timer necessary
 						resetKeyPresses();	
-						chrome.runtime.sendMessage({msg: 'new_tab_no_follow', url: keysurfHelpers.getKeymapValue(pressedChar, keysurfHelpers.altPressed)}, function(response) {});
+						chrome.runtime.sendMessage({msg: 'new_tab_no_follow', url: webMotionHelpers.getKeymapValue(pressedChar, webMotionHelpers.altPressed)}, function(response) {});
 					}
 					else {
 						//consider this identical to the first one, ie consider the keys separate despite they were the same
 						// set timeout to follow link in THIS tab.
 						var timeOutID = window.setTimeout(function(localChar) {
 							resetKeyPresses();
-							window.location = keysurfHelpers.getKeymapValue(localChar, keysurfHelpers.altPressed);
+							window.location = webMotionHelpers.getKeymapValue(localChar, webMotionHelpers.altPressed);
 						}, 300, pressedChar);
 						keyPresses[keyPresses.length - 1].timeOutID = timeOutID;
 					}
@@ -171,7 +171,7 @@
 	}
 
 	function resetAllLinks() {
-		$('keysurf').each(function(index, value) {
+		$('webmotion').each(function(index, value) {
 			$(this).replaceWith($(this).text());		
 		});
 	}
@@ -179,7 +179,7 @@
 	function gatherLegitimateLinks() {
 		var modifiableLinks = [];
 		$('a:visible').each(function(index) {				
-			if (keysurfHelpers.isLinkLegitimate($(this))) {
+			if (webMotionHelpers.isLinkLegitimate($(this))) {
 				var link = {linkObj:$(this), fontSize: parseInt($(this).css('font-size')), shortCut:"", originalOrder:index, absoluteURL: $(this).prop("href")};
 				modifiableLinks.push(link);
 			}		
@@ -202,11 +202,9 @@
 
 	function initializeWindowScrollListener() {
 		$(window).scroll(function() {
-			console.log(1);
 			clearTimeout($.data(this, 'scrollTimer'));
 			$.data(this, 'scrollTimer', setTimeout(function() {
         		// do something
-        		console.log(2);
         		processLinks();
         	}, 70));
 		});
--- webMotionHelpers.js
+++ webMotionHelpers.js
@@ -1,43 +1,43 @@
-var keysurfHelpers = (function() {
+var webMotionHelpers = (function() {
 
-	var _keysurfHelpers = {};
-	_keysurfHelpers.ctrlPressed = false;
-	_keysurfHelpers.shiftPressed = false;
-	_keysurfHelpers.altPressed = false;
-	_keysurfHelpers.cmdPressed = false;
-	_keysurfHelpers.standardColor = '#e84c3d';
-	_keysurfHelpers.alternativeColor = '#5280bb';
-	_keysurfHelpers.viewPortHeight;
-	_keysurfHelpers.keyMap = {}; // maps the keys to the URLs
-	_keysurfHelpers.keyMapAlt = {}; 
-	_keysurfHelpers.takenAbbreviations = []; // maintains a list of all the user up letter / letter combos
-	_keysurfHelpers.takenAbbreviationsAlt = []; 
+	var _webMotionHelpers = {};
+	_webMotionHelpers.ctrlPressed = false;
+	_webMotionHelpers.shiftPressed = false;
+	_webMotionHelpers.altPressed = false;
+	_webMotionHelpers.cmdPressed = false;
+	_webMotionHelpers.standardColor = '#e84c3d';
+	_webMotionHelpers.alternativeColor = '#5280bb';
+	_webMotionHelpers.viewPortHeight;
+	_webMotionHelpers.keyMap = {}; // maps the keys to the URLs
+	_webMotionHelpers.keyMapAlt = {}; 
+	_webMotionHelpers.takenAbbreviations = []; // maintains a list of all the user up letter / letter combos
+	_webMotionHelpers.takenAbbreviationsAlt = []; 
 
 	//these we use these to close website, go back and forth between tabs, etc.
-	_keysurfHelpers.reservedShortcuts = ['x', 'b', 'h', 'j', 'k', 'l'];
-	_keysurfHelpers.alwaysPermissibleShortcuts = ['h', 'l']; // even in forbidden domains (basically just left and right)
-	_keysurfHelpers.forbiddenDomains = ['gmail','google', 'facebook.com', 'twitter.com', , 'notezilla.io', '0.0.0.0'];
+	_webMotionHelpers.reservedShortcuts = ['x', 'b', 'h', 'j', 'k', 'l'];
+	_webMotionHelpers.alwaysPermissibleShortcuts = ['h', 'l']; // even in forbidden domains (basically just left and right)
+	_webMotionHelpers.forbiddenDomains = ['gmail','google', 'facebook.com', 'twitter.com', , 'notezilla.io', '0.0.0.0'];
 
 
-	_keysurfHelpers.specialCharactersPressed = function() {
+	_webMotionHelpers.specialCharactersPressed = function() {
 		// console.log('*****');
-		// console.log(_keysurfHelpers.ctrlPressed);
-		// console.log(_keysurfHelpers.shiftPressed);
-		// console.log(_keysurfHelpers.altPressed);
-		// console.log(_keysurfHelpers.cmdPressed);
-		return ((_keysurfHelpers.ctrlPressed) || (_keysurfHelpers.shiftPressed) || (_keysurfHelpers.altPressed) || (_keysurfHelpers.cmdPressed));
+		// console.log(_webMotionHelpers.ctrlPressed);
+		// console.log(_webMotionHelpers.shiftPressed);
+		// console.log(_webMotionHelpers.altPressed);
+		// console.log(_webMotionHelpers.cmdPressed);
+		return ((_webMotionHelpers.ctrlPressed) || (_webMotionHelpers.shiftPressed) || (_webMotionHelpers.altPressed) || (_webMotionHelpers.cmdPressed));
 	}
 
-	_keysurfHelpers.htmlDecode = function(str) {
+	_webMotionHelpers.htmlDecode = function(str) {
 		var e = document.createElement('div');
 		e.innerHTML = str;
 		return e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
 	}
 
-	_keysurfHelpers.analyzeAndModifyLink = function(linkObj, alternative) {
+	_webMotionHelpers.analyzeAndModifyLink = function(linkObj, alternative) {
 		// Figures out who the possible candiates are inside the link text, and maps them to their position in the innerHTML of that link.
 		// If alternative == true, then everything will be pushed to the 'backup' arrays used when user presses ALT key
-		var letterMappings = _keysurfHelpers.genereateLetterToHTMLMapping(linkObj); // contains the text() mapped to the underlying HTML
+		var letterMappings = _webMotionHelpers.genereateLetterToHTMLMapping(linkObj); // contains the text() mapped to the underlying HTML
 		if (letterMappings.length == 0) {
 			// no available text inside link. Could be image. For now, do nothing.
 			return false; 
@@ -55,32 +55,32 @@ var keysurfHelpers = (function() {
 				chosenLetterOrigPos = letterMappings[letterIndex].originalPosition;
 				pushToTakenAbbreviations(chosenLetter, alternative);
 				var existingInnerHTML = linkObj.html();
-				var colorToUse = _keysurfHelpers.standardColor;
-				var deltaE = _keysurfHelpers.colorDistance(colorToUse,linkObj.css('color'));
+				var colorToUse = _webMotionHelpers.standardColor;
+				var deltaE = _webMotionHelpers.colorDistance(colorToUse,linkObj.css('color'));
 				if (deltaE < 50) {
-					colorToUse = _keysurfHelpers.alternativeColor;
+					colorToUse = _webMotionHelpers.alternativeColor;
 				}
-				if (_keysurfHelpers.hasBackgroundColorProperty(linkObj)) {
-					var deltaE = _keysurfHelpers.colorDistance(_keysurfHelpers.standardColor,linkObj.css('background-color'));
+				if (_webMotionHelpers.hasBackgroundColorProperty(linkObj)) {
+					var deltaE = _webMotionHelpers.colorDistance(_webMotionHelpers.standardColor,linkObj.css('background-color'));
 					if (deltaE < 50) {
-						colorToUse = _keysurfHelpers.alternativeColor;
+						colorToUse = _webMotionHelpers.alternativeColor;
 					}
 				}
 				else {
 					// get all the background elements of the parents.
-					var parentWithBG = _keysurfHelpers.getFirstParentElementWithBGProperty(linkObj);
+					var parentWithBG = _webMotionHelpers.getFirstParentElementWithBGProperty(linkObj);
 					if (parentWithBG) {
-						var deltaE = _keysurfHelpers.colorDistance(_keysurfHelpers.standardColor,parentWithBG.css('background-color'));
+						var deltaE = _webMotionHelpers.colorDistance(_webMotionHelpers.standardColor,parentWithBG.css('background-color'));
 						if (deltaE < 50) {
-							colorToUse = _keysurfHelpers.alternativeColor;
+							colorToUse = _webMotionHelpers.alternativeColor;
 						}
 					}
 				}
 				if (alternative) {
-					newInnerHTML = existingInnerHTML.replaceAt(chosenLetterOrigPos, "<keysurf data-modified-color='"+colorToUse+"' data-original-color='"+linkObj.css('color')+"' data-original-fontweight='"+linkObj.css('font-weight')+"' class='alternative' style=\"\">"+linkObj.html()[chosenLetterOrigPos]+"</keysurf>");
+					newInnerHTML = existingInnerHTML.replaceAt(chosenLetterOrigPos, "<webmotion data-modified-color='"+colorToUse+"' data-original-color='"+linkObj.css('color')+"' data-original-fontweight='"+linkObj.css('font-weight')+"' class='alternative' style=\"\">"+linkObj.html()[chosenLetterOrigPos]+"</webmotion>");
 				}
 				else {
-					newInnerHTML = existingInnerHTML.replaceAt(chosenLetterOrigPos, "<keysurf data-modified-color='"+colorToUse+"' data-original-color='"+linkObj.css('color')+"' data-original-fontweight='"+linkObj.css('font-weight')+"' class='regular' style=\"color:"+colorToUse+"; font-weight:bold;\">"+linkObj.html()[chosenLetterOrigPos]+"</keysurf>");
+					newInnerHTML = existingInnerHTML.replaceAt(chosenLetterOrigPos, "<webmotion data-modified-color='"+colorToUse+"' data-original-color='"+linkObj.css('color')+"' data-original-fontweight='"+linkObj.css('font-weight')+"' class='regular' style=\"color:"+colorToUse+"; font-weight:bold;\">"+linkObj.html()[chosenLetterOrigPos]+"</webmotion>");
 				}
 				
 				linkObj.html(newInnerHTML);
@@ -93,29 +93,29 @@ var keysurfHelpers = (function() {
 		}
 	}
 
-	_keysurfHelpers.initializeSpecialKeyListeners = function() {
+	_webMotionHelpers.initializeSpecialKeyListeners = function() {
 
 		$(document).on("keydown keyup", function(e) {
 
-			_keysurfHelpers.cmdPressed = (e.keyCode == 91 || e.keyCode == 93);
+			_webMotionHelpers.cmdPressed = (e.keyCode == 91 || e.keyCode == 93);
 
 		});
 		$(document).on("keyup keydown", function(e) {
-			_keysurfHelpers.shiftPressed = (e.keyCode == 16);
+			_webMotionHelpers.shiftPressed = (e.keyCode == 16);
 		});
 		$(document).on("keyup keydown", function(e) {
-			_keysurfHelpers.ctrlPressed = (e.keyCode == 17);
+			_webMotionHelpers.ctrlPressed = (e.keyCode == 17);
 		});
 
 		$(document).on("keydown", function(e) {
 			if (e.keyCode == 18) {
-				_keysurfHelpers.altPressed = true;
-				$('keysurf.regular').each(function() {
+				_webMotionHelpers.altPressed = true;
+				$('webmotion.regular').each(function() {
 					var originalColor = $(this).attr('data-original-color');
 					var originalFontweight = $(this).attr('data-original-fontweight');
 					$(this).css('color', originalColor).css('font-weight', originalFontweight);
 				});
-				$('keysurf.alternative').each(function() {
+				$('webmotion.alternative').each(function() {
 					var newColor = $(this).attr('data-modified-color');
 					$(this).css('color', newColor).css('font-weight', 'bold');
 				});
@@ -124,13 +124,13 @@ var keysurfHelpers = (function() {
 
 		$(document).on("keyup", function(e) {
 			if (e.keyCode == 18) {
-				_keysurfHelpers.altPressed = false;
-				$('keysurf.alternative').each(function() {
+				_webMotionHelpers.altPressed = false;
+				$('webmotion.alternative').each(function() {
 					var originalColor = $(this).attr('data-original-color');
 					var originalFontweight = $(this).attr('data-original-fontweight');
 					$(this).css('color', originalColor).css('font-weight', originalFontweight);
 				});
-				$('keysurf.regular').each(function() {
+				$('webmotion.regular').each(function() {
 					var newColor = $(this).attr('data-modified-color');
 					$(this).css('color', newColor).css('font-weight', 'bold');
 				});
@@ -139,29 +139,29 @@ var keysurfHelpers = (function() {
 
 	}
 
-	_keysurfHelpers.getFirstParentElementWithBGProperty = function(elem) {
+	_webMotionHelpers.getFirstParentElementWithBGProperty = function(elem) {
 		var allParents = elem.parents();
 		var counter;
 		for (counter = 0; counter <= allParents.length - 1; counter++) {
-			if (_keysurfHelpers.hasBackgroundColorProperty($(allParents[counter]))) {
+			if (_webMotionHelpers.hasBackgroundColorProperty($(allParents[counter]))) {
 				return $(allParents[counter]);
 			}
 		}
 		return false;
 	}	
 
-	_keysurfHelpers.hasBackgroundColorProperty = function(elem) {
+	_webMotionHelpers.hasBackgroundColorProperty = function(elem) {
 		// if we do this the traditional elem.css('background-color')
 		// way, seems we are getting rgb(0,0,0) as a response even if it's 
 		// not set.
 		return (elem.css('background-color') != 'rgba(0, 0, 0, 0)');
 	}
 
-	_keysurfHelpers.hasWhiteBackground = function(elem) {
+	_webMotionHelpers.hasWhiteBackground = function(elem) {
 		return (elem.css('background-color').containsString("rgb(0, 0, 0)") || elem.css('background-color').containsString("rgba(0, 0, 0, 0)"));
 	}
 
-	_keysurfHelpers.isDomainAllowed = function() {
+	_webMotionHelpers.isDomainAllowed = function() {
 		var domainAllowed = true;
 		for (var domainCounter = 0; domainCounter <= this.forbiddenDomains.length - 1; domainCounter++) {
 			if (document.domain.indexOf(this.forbiddenDomains[domainCounter]) != -1) {
@@ -173,7 +173,7 @@ var keysurfHelpers = (function() {
 	}
 
 
-	_keysurfHelpers.colorDistance = function(color1, color2) {
+	_webMotionHelpers.colorDistance = function(color1, color2) {
 		// takes two colors as inputs. Strings are fine. RGB assumed.
 		// converts the two colors to LAB space by using Shushik/i-color/ library
 		// then calculates Delta-E, which represents the euclidian distance between them.
@@ -187,7 +187,7 @@ var keysurfHelpers = (function() {
 		return Math.sqrt((Math.pow(lDiff, 2) + Math.pow(aDiff, 2) + Math.pow(bDiff, 2)));
 	}	
 
-	_keysurfHelpers.initializeFocusBlurListeners = function() {
+	_webMotionHelpers.initializeFocusBlurListeners = function() {
 
 		window.addEventListener("focus", function(event) { 
 			this.cmdPressed = false;
@@ -207,7 +207,7 @@ var keysurfHelpers = (function() {
 	}
 
 
-	_keysurfHelpers.isElementInView = function(elem) {
+	_webMotionHelpers.isElementInView = function(elem) {
 		var docViewTop = $(window).scrollTop();
 		var docViewBottom = docViewTop + this.viewPortHeight;
 		var elemTop = $(elem).offset().top;
@@ -215,17 +215,17 @@ var keysurfHelpers = (function() {
 		return ((elemBottom <= docViewBottom) && (elemTop >= docViewTop));
 	}
 
-	_keysurfHelpers.isAlphanumeric = function(str) {
+	_webMotionHelpers.isAlphanumeric = function(str) {
 		return /[a-z0-9A-Z]/.test(str);
 	}
 
-	_keysurfHelpers.noInputFieldsActive = function() {
+	_webMotionHelpers.noInputFieldsActive = function() {
 		// true if user is currently typing inside a text field or a text area.
 		var el = document.activeElement;
 		return !(el && ((el.tagName.toLowerCase() == 'input' && (el.type == 'text' || el.type == 'password' || el.type == 'email' || el.type == 'search' || el.type == 'url' || el.type == 'tel' || el.type == 'time' || el.type == 'number' )) || el.tagName.toLowerCase() == 'textarea'));
 	}
 	
-	_keysurfHelpers.scrollWindow = function(px) {
+	_webMotionHelpers.scrollWindow = function(px) {
 		var sign = '+';
 		if (px < 0) {
 			sign = '-';
@@ -240,7 +240,7 @@ var keysurfHelpers = (function() {
 		});
 	}
 
-	_keysurfHelpers.isLinkLegitimate = function(DOMElem) {
+	_webMotionHelpers.isLinkLegitimate = function(DOMElem) {
 		// To be legitimate: dom element / link must meet the following criteria:
 		// 1) Be in view
 		// 2) have non blank href attribute
@@ -259,7 +259,7 @@ var keysurfHelpers = (function() {
 	}
 
 
-	_keysurfHelpers.genereateLetterToHTMLMapping = function(linkDOMElem) {
+	_webMotionHelpers.genereateLetterToHTMLMapping = function(linkDOMElem) {
 		/*
 		This method takes a link DOM element from jQuery (ie <a id='test' href='/xyz'>Hello there!</a>, etc) and looks at the html / text inside the link (in this example: "Hello there!"), 
 		and figures out which of the letters in that string are actually candidates for being replaced with a hotkey (and formatted as red). 
@@ -268,7 +268,7 @@ var keysurfHelpers = (function() {
 		just text but rather an html expression, it still works: for example innerHTML is "<b>Hello</b>": 'h' -> 3
 
 		It removes duplicates, lower cases everything, and ignores non-alphanumeric characters. It also removes any of the letters considered a reserved shortcut, 
-		as defined above by _keysurfHelpers.reservedShortcuts.
+		as defined above by _webMotionHelpers.reservedShortcuts.
 
 		The actual output of the above example will look like this:
 
@@ -290,7 +290,7 @@ var keysurfHelpers = (function() {
 				// has an offset of the length of the first, and the third one has an offset of the 2 first ones
 				// used to be this!!!!
 				nodesContainer.push({txt: $(this).text(), html: $(this).text(), charOffset: nodesCumulativeHTMLLength});
-				// nodesContainer.push({txt: $(this).text(), html: _keysurfHelpers.htmlDecode($(this).context.innerText), charOffset: nodesCumulativeHTMLLength});
+				// nodesContainer.push({txt: $(this).text(), html: _webMotionHelpers.htmlDecode($(this).context.innerText), charOffset: nodesCumulativeHTMLLength});
 				nodesCumulativeHTMLLength += $(this).text().length;
 			}
 			else {
@@ -332,49 +332,49 @@ var keysurfHelpers = (function() {
 	// A bunch of getters and setters in lieu of accessing variables by reference (which is not possible in Javascript)
 	function getTakenAbbreviations(alternative) {
 		if (alternative) {
-			return _keysurfHelpers.takenAbbreviationsAlt;		
+			return _webMotionHelpers.takenAbbreviationsAlt;		
 		}	
 		else {
-			return _keysurfHelpers.takenAbbreviations;		
+			return _webMotionHelpers.takenAbbreviations;		
 		}
 	}
 	function pushToTakenAbbreviations(chosenLetter, alternative) {
 		if (alternative) {
-			_keysurfHelpers.takenAbbreviationsAlt.push(chosenLetter);		
+			_webMotionHelpers.takenAbbreviationsAlt.push(chosenLetter);		
 		}	
 		else {
-			_keysurfHelpers.takenAbbreviations.push(chosenLetter);		
+			_webMotionHelpers.takenAbbreviations.push(chosenLetter);		
 		}
 	}
 
 	function setKeymapKeyValue(key, value, alternative) {
 		if (alternative) {
-			_keysurfHelpers.keyMapAlt[key] = value;
+			_webMotionHelpers.keyMapAlt[key] = value;
 		}
 		else {
-			_keysurfHelpers.keyMap[key] = value;
+			_webMotionHelpers.keyMap[key] = value;
 		} 
 	}
 
-	_keysurfHelpers.getKeymap = function(alternative) {
+	_webMotionHelpers.getKeymap = function(alternative) {
 		if (alternative) {
-			return _keysurfHelpers.keyMapAlt;
+			return _webMotionHelpers.keyMapAlt;
 		}
 		else {
-			return _keysurfHelpers.keyMap;
+			return _webMotionHelpers.keyMap;
 		} 
 	}
 
 
-	_keysurfHelpers.getKeymapValue = function(key, alternative) {
+	_webMotionHelpers.getKeymapValue = function(key, alternative) {
 		if (alternative) {
-			return _keysurfHelpers.keyMapAlt[key];
+			return _webMotionHelpers.keyMapAlt[key];
 		}
 		else {
-			return _keysurfHelpers.keyMap[key];
+			return _webMotionHelpers.keyMap[key];
 		} 
 	}
 
-	return _keysurfHelpers;
+	return _webMotionHelpers;
 
 }());
